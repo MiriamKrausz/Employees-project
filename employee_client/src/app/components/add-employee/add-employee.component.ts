2f15beff86bef63e23344ceb945262e39a0de4c6
miri <mk6764576@gmail.com>
2024-04-07 12:49:50 +0300

    snackBar

--- employee_client/src/app/components/add-employee/add-employee.component.ts
+++ employee_client/src/app/components/add-employee/add-employee.component.ts
@@ -1,7 +1,8 @@
 
 import { Component, OnInit } from '@angular/core';
-import {AbstractControl, FormArray, FormBuilder, FormGroup,ValidationErrors,Validators } from '@angular/forms';
+import {AbstractControl, FormArray, FormBuilder, FormGroup,Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar'; // import for snackbar
 import { Position } from '../../models/position.medel';
 import { EmployeeService } from '../../services/employee.service';
 import { PositionService } from '../../services/position.service';
@@ -18,7 +19,6 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { DateAdapter, MAT_DATE_FORMATS, MAT_NATIVE_DATE_FORMATS, NativeDateAdapter } from '@angular/material/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-add-employee',
@@ -26,7 +26,7 @@ import { Observable, of } from 'rxjs';
   imports: [CommonModule,MatInputModule,MatFormFieldModule,MatCheckboxModule,MatExpansionModule,MatIconModule,MatDialogModule,MatButtonModule,MatSelectModule,ReactiveFormsModule,MatDatepickerModule,MatSlideToggleModule],
   templateUrl: './add-employee.component.html',
   styleUrls: ['./add-employee.component.scss'],
-    providers: [
+  providers: [
     { provide: MAT_DATE_FORMATS, useValue: MAT_NATIVE_DATE_FORMATS },
     { provide: DateAdapter, useClass: NativeDateAdapter }
   ]
@@ -41,7 +41,8 @@ export class AddEmployeeComponent implements OnInit {
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<AddEmployeeComponent>,
     private _employeeService: EmployeeService,
-    private _positionService: PositionService
+    private _positionService: PositionService,
+    private _snackBar: MatSnackBar // Inject MatSnackBar
   ) { }
 
   ngOnInit(): void {
@@ -49,18 +50,20 @@ export class AddEmployeeComponent implements OnInit {
     this.loadPositions();
   }
 
+  // Function to initialize form
   initializeForm(): void {
     this.employeeForm = this.fb.group({
       firstName: ['', Validators.required],
       surname: ['', Validators.required],
       identityNumber: ['', [Validators.required, Validators.pattern(/^\d{9}$/)]],
       gender: ['', Validators.required],
-      dateOfBirth: ['', Validators.required,this.ageValidator],
+      dateOfBirth: ['', [Validators.required,this.ageValidator()]],
       beginningOfWork: ['', Validators.required],
       positions: this.fb.array([], Validators.required)
     });
   }
 
+  // Function to add a position control to the form array
   addPositionControl(): void {
     this.positionsFormArray.push(this.fb.group({
       positionId: ['',Validators.required],
@@ -68,50 +71,18 @@ export class AddEmployeeComponent implements OnInit {
       entryDate: ['',[Validators.required, this.entryDateValidator()]]
     }));
   }
-  // addPositionControl(): void {
-  //   this.positionsFormArray.push(this.fb.group({
-  //     positionId: ['', Validators.required],
-  //     isManagement: [false],
-  //     entryDate: ['', [Validators.required, this.entryDateValidator()]]}));
-  // }
-  
 
- ageValidator(control: AbstractControl): 
-  Observable<ValidationErrors | null> {
-  const birthDate = new Date(control.value);
-  const today = new Date();
-  const age = today.getFullYear() - birthDate.getFullYear();
-      if (age < 18) {
-    // Emit an object with a validation error.
-      return of({ underage: true});
-    }
-    // Emit null, to indicate no error occurred.
-    return of(null);
+  // Age validator function
+  ageValidator() {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const birthDate = new Date(control.value);
+      const today = new Date();
+      const age = today.getFullYear() - birthDate.getFullYear();
+      return age >= 18 ? null : { 'underage': true };
+    };
   }
 
-  // ageValidator() {
-  //   return (control: AbstractControl): { [key: string]: any } | null => {
-  //     const birthDate = new Date(control.value);
-  // const today = new Date();
-  // const age = today.getFullYear() - birthDate.getFullYear();
-  //     return age >= 18 ? null : { 'underage': true };
-  //   };
-  // }
-
-  // entryDateAfterWorkDateValidator(control: AbstractControl): 
-  // Observable<ValidationErrors | null> {
-  //   const workDate = (control.root as FormGroup).get('beginningOfWork')?.value;
-  //   const entryDate = control.value;
-  //   console.log("workDate",workDate);
-  //   console.log("entryDate",entryDate);
-  //     if (workDate && entryDate && workDate.getTime() > entryDate.getTime()) {
-  //   // Emit an object with a validation error.
-  //     return of({ entryDateBeforeWorkDate: true});
-  //   }
-  //   // Emit null, to indicate no error occurred.
-  //   return of(null);
-  // }
-
+  // Entry date validator function
   entryDateValidator() {
     return (control: AbstractControl): { [key: string]: any } | null => {
       const entryDate = new Date(control.value);
@@ -119,10 +90,13 @@ export class AddEmployeeComponent implements OnInit {
       return entryDate >= workDate ? null : { 'entryDateInvalid': true };
     };
   }
+
+  // Getter for positions form array
   get positionsFormArray(): FormArray {
     return this.employeeForm.get('positions') as FormArray;
   }
 
+  // Function to load positions from service
   loadPositions(): void {
     this._positionService.getAllPositions().subscribe(positions => {
       this.positions = positions;
@@ -130,27 +104,32 @@ export class AddEmployeeComponent implements OnInit {
     });
   }
 
+  // Function to remove a position control from the form array
   removePositionControl(index: number): void {
     this.positionsFormArray.removeAt(index);
   }
 
+  // Function to check if a position is disabled
   isPositionDisabled(positionId: number, index: number): boolean {
     const selectedPositions = this.employeeForm.value.positions.map((pos: any) => pos.positionId);
     return selectedPositions.includes(positionId) && selectedPositions.indexOf(positionId) !== index;
   }
 
+  // Function to handle form submission
   submit(): void {
     if (this.employeeForm.valid) {
       const formData = this.employeeForm.value;
       console.log("formData", formData);
       this._employeeService.addEmployee(formData).subscribe(() => {
         console.log('saving');
+        this.openSnackBar('Employee added successfully'); // Open snackbar on success
         this.dialogRef.close(true);    
       }, error => {
         if (error.status === 400) {
           this.errors = error.error.errors;
         }
         console.error('Error adding employee:', error);
+        this.openSnackBar('Error adding employee'); // Open snackbar on error
         console.log('Server validation errors:', error.error.errors);
       });
     } else {
@@ -159,10 +138,18 @@ export class AddEmployeeComponent implements OnInit {
     }
   }
 
-
-
+  // Function to handle cancellation
   cancel(): void {
     this.dialogRef.close();
+  }
+
+  // Function to open snackbar
+  openSnackBar(message: string): void {
+    this._snackBar.open(message, 'Close', {
+      duration: 3000, // Duration in milliseconds
+      horizontalPosition: 'end', // Positioning the snackbar
+      verticalPosition: 'bottom'
+    });
   }
 }
 
--- employee_client/src/app/components/edit-employee/edit-employee.component.ts
+++ employee_client/src/app/components/edit-employee/edit-employee.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
-import { AbstractControl,AsyncValidatorFn, FormArray, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar'; // import for snackbar
 import { Employee } from '../../models/employee.model';
 import { EmployeeService } from '../../services/employee.service';
 import { MatDialogContent } from '@angular/material/dialog'; 
@@ -17,9 +18,7 @@ import { Position } from '../../models/position.medel';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { DateAdapter, MAT_DATE_FORMATS, MAT_NATIVE_DATE_FORMATS, NativeDateAdapter} from '@angular/material/core';
-import { Observable } from 'rxjs';
 
 
 @Component({
@@ -51,18 +50,19 @@ export class EditEmployeeComponent {
     this.loadPositions();
   }
 
+  // Function to initialize form
   initializeForm(): void {
     const employee = this.data.employee;
     this.employeeForm = this.fb.group({
-        firstName: [employee.firstName, Validators.required],
-        surname: [employee.surname, Validators.required],
-        identityNumber: [employee.identityNumber],
-        gender: [employee.gender, Validators.required],
-        dateOfBirth: [employee.dateOfBirth, [Validators.required, this.ageValidator]],
-        beginningOfWork: [employee.beginningOfWork, Validators.required],
-        positions: this.fb.array([]) // Initialize positions array, modify if necessary
+      firstName: [employee.firstName, Validators.required],
+      surname: [employee.surname, Validators.required],
+      identityNumber: [employee.identityNumber],
+      gender: [employee.gender, Validators.required],
+      dateOfBirth: [employee.dateOfBirth, [Validators.required, this.ageValidator()]],
+      beginningOfWork: [employee.beginningOfWork, Validators.required],
+      positions: this.fb.array([]) // Initialize positions array, modify if necessary
     });     
-   
+    
     // Initialize positions controls with default values
     if (employee.positions && employee.positions.length > 0) {
       employee.positions.forEach(position => {
@@ -74,94 +74,99 @@ export class EditEmployeeComponent {
     }
   }  
 
+  // Function to add a position control to the form array
+  addPositionControl(positionId: number = null, isAdministrative: boolean = false, entryDate: Date = null): void {
+    this.positionsFormArray.push(this.fb.group({       
+      positionId: [positionId, Validators.required],
+      isAdministrative: [isAdministrative, Validators.required],
+      entryDate: [entryDate, [Validators.required, this.entryDateValidator()]]
+    }));
+  }
 
+  // Getter for positions form array
   get positionsFormArray(): FormArray {
     return this.employeeForm.get('positions') as FormArray;
   }
 
-  ageValidator(control: AbstractControl): ValidationErrors | null {
-    const birthDate = new Date(control.value);
-    const today = new Date();
-    const age = today.getFullYear() - birthDate.getFullYear();
-    if (age < 18) {
-      return { underage: true };
-    }
-    return null;
+  // Age validator function
+  ageValidator() {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const birthDate = new Date(control.value);
+      const today = new Date();
+      const age = today.getFullYear() - birthDate.getFullYear();
+      return age >= 18 ? null : { 'underage': true };
+    };
   }
 
+  // Entry date validator function
+  entryDateValidator() {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const entryDate = new Date(control.value);
+      const workDate = new Date(this.employeeForm.get('beginningOfWork').value);
+      return entryDate >= workDate ? null : { 'entryDateInvalid': true };
+    };
+  }
 
-  entryDateValidator: AsyncValidatorFn = (control: AbstractControl): Observable<ValidationErrors | null> => {
-    return new Observable(observer => {
-      setTimeout(() => {
-        const entryDate = new Date(control.value);
-        const beginningOfWork = new Date(this.employeeForm.get('beginningOfWork').value);
-        if (entryDate < beginningOfWork) {
-          observer.next({ invalidEntryDate: true });
-        } else {
-          observer.next(null);
-        }
-        observer.complete();
-      }, 0);
-    });
-  };
-
-
+  // Function to load positions from service
   loadPositions(): void {
     this._positionService.getAllPositions().subscribe(positions => {
       this.positions = positions;
-    }, error => {
-      console.error('Error loading positions:', error);
     });
   }
 
-  addPositionControl(positionId: number =null, isAdministrative: boolean = false, entryDate: Date = null): void {
-     console.log(positionId, isAdministrative, entryDate); 
-    this.positionsFormArray.push(this.fb.group({       
-      positionId: [positionId, Validators.required],
-      isAdministrative: [isAdministrative, Validators.required],
-      entryDate: [entryDate, Validators.required,this.entryDateValidator]
-    }))
-  }
-
+  // Function to remove a position control from the form array
   removePositionControl(index: number): void {
     this.positionsFormArray.removeAt(index);
   }
 
+  // Function to check if a position is disabled
   isPositionDisabled(positionId: number, index: number): boolean {
     const selectedPositions = this.employeeForm.value.positions.map((pos: any) => pos.positionId);
     return selectedPositions.includes(positionId) && selectedPositions.indexOf(positionId) !== index;
   }
 
-  cancel(): void {
-    this.dialogRef.close();
+
+  // Function to handle form submission
+  submit(): void {
+    if (this.employeeForm.valid) {
+      const employeeId = this.data.employee.id;
+      this._employeeService.getEmployeeById(employeeId).subscribe((employee: Employee) => {
+        const updatedEmployee = {
+          ...employee,
+          ...this.employeeForm.value
+        };
+        this._employeeService.updateEmployee(updatedEmployee).subscribe(() => {
+          console.log('Employee updated successfully');
+          this.openSnackBar('Employee updated successfully'); // Open snackbar on success
+          this.dialogRef.close(true);
+        }, error => {
+          console.error('Error updating employee:', error);
+          this.openSnackBar('Error updating employee'); // Open snackbar on error
+        });
+      }, error => {
+        console.error('Error getting employee by ID:', error);
+        this.openSnackBar('Error getting employee by ID'); // Open snackbar on error
+      });
+    } else {
+      this.employeeForm.markAllAsTouched();
+      console.error('Form is not valid');
+      this.openSnackBar('Form is not valid'); // Open snackbar if form is not valid
+    }
   }
 
-  
+  // Function to open snackbar
+  openSnackBar(message: string): void {
+    this._snackBar.open(message, 'Close', {
+      duration: 3000, // Duration in milliseconds
+      horizontalPosition: 'end', // Positioning the snackbar
+      verticalPosition: 'bottom'
+    });
+  }
 
-submit(): void {
-    if (this.employeeForm.valid) {
-        const employeeId = this.data.employee.id;
-        this._employeeService.getEmployeeById(employeeId).subscribe((employee: Employee) => {
-            const updatedEmployee = {
-                ...employee,
-                ...this.employeeForm.value
-            };
-            this._employeeService.updateEmployee(updatedEmployee).subscribe(() => {
-                console.log('Employee updated successfully');
-                this._snackBar.open('Object updated successfully', 'Close', {
-                    duration: 3000, // Set the duration for the snack-bar message
-                });
-                this.dialogRef.close(true);
-            }, error => {
-                console.error('Error updating employee:', error);
-            });
-        }, error => {
-            console.error('Error getting employee by ID:', error);
-        });
-    } else {
-        this.employeeForm.markAllAsTouched();
-        console.error('Form is not valid');
+
+    // Function to handle cancellation
+    cancel(): void {
+      this.dialogRef.close();
     }
-}
-
+  
 }
